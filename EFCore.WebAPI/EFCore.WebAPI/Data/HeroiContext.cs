486690550b65e3c8917ede5c297fc1ec402ae0c4
RRDYU <dyu.rocha.rodrigues@gmail.com>
2022-04-03 22:43:02 -0300

    Iniciando Projeto

---
+++ EFCore.WebAPI/EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=sa0140536066;Persist Security Info=False;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-3UEHHIK/SQLEXPRESS");
+        }
+       
+
+
+
+
+
+    }
+}
---
+++ EFCore.WebAPI/EFCore.WebAPI/Migrations/20220401011641_Inicial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.WebAPI.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Batalhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DtInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DtFim = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batalhas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Herois",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BatalhaIdId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Herois", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Herois_Batalhas_BatalhaIdId",
+                        column: x => x.BatalhaIdId,
+                        principalTable: "Batalhas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Armas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HeroiIdId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Armas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Armas_Herois_HeroiIdId",
+                        column: x => x.HeroiIdId,
+                        principalTable: "Herois",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Armas_HeroiIdId",
+                table: "Armas",
+                column: "HeroiIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Herois_BatalhaIdId",
+                table: "Herois",
+                column: "BatalhaIdId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Armas");
+
+            migrationBuilder.DropTable(
+                name: "Herois");
+
+            migrationBuilder.DropTable(
+                name: "Batalhas");
+        }
+    }
+}
